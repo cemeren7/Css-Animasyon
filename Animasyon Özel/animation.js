087bb94f35881386173ce8672ad7e0e20fd39ab6
cem eren yardımcı <er7tr1245@gmail.com>
2024-12-02 19:53:26 +0300

    Css-Animasyon
    Css ve javascript kullanarak tasarladığım basit bir animasyon

---
+++ Animasyon Özel/animation.js
@@ -0,0 +1,11 @@
+function animaiton(){
+    document.getElementById("img").style.animation="none"
+    document.getElementById("p").innerHTML="Animasyon Duraklatıldı"
+    document.getElementById("p").style.display="block"
+}
+
+function animaitonstart(){
+    document.getElementById("img").style.animation="images 6s infinite"
+    document.getElementById("p").innerHTML="Animasyon Başlatıldı"
+    document.getElementById("p").style.display="block"
+}
